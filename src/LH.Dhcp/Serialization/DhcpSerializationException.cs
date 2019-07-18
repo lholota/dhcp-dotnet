37195102ed63954ed214c68e3cd5f1d05e7592d6
Lukas Holota <l.holota@outlook.com>
2019-07-18 16:54:26 +0200

    Fix

--- src/LH.Dhcp/Serialization/DhcpSerializationException.cs
+++ src/LH.Dhcp/Serialization/DhcpSerializationException.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace LH.Dhcp.Serialization
 {
+    [Serializable]
     public class DhcpSerializationException : Exception
     {
         public DhcpSerializationException(string message, Exception inner = null)
             : base(message, inner)
+        {
+        }
+
+        protected DhcpSerializationException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
         {
         }
     }
